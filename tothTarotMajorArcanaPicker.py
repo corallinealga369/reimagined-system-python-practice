d49419eec56174ff70277396f36658964ee0f1a4
coralline <57780599+corallinealga369@users.noreply.github.com>
2019-11-17 16:43:57 -0700

    WIP update thothTarotCardPicker.py
    I have been unsuccessful adding date so far but have some write to file functionality.

--- tothTarotMajorArcanaPicker.py
+++ tothTarotMajorArcanaPicker.py
@@ -3,6 +3,17 @@
 #we need to make a random choice
 import random
 
+#I want to be able to read and write from a file
+import csv
+
+#I want to write the date to a file
+import datetime
+
+cards=0
+MyCards=[]
+
+#today's date
+#ThisDate=datetime.datetime.now
 #This is a dictionary of all the major arcana in the Thoth Tarot
 MajorArcana = {
     0: "The Fool",
@@ -34,21 +45,45 @@ Reading=input("Enter 1 to draw a daily card, enter 3 to do a past present future
 
 #if the user chose 1 card draw one card and print it
 if int(Reading) == 1:
+    
     MyCard=random.choice(MajorArcana)
     print(MyCard)
+    MyCards.append(MyCard)
+    notes=input("Would you like to add a note? Y/N:")
+    if notes=="Y" or notes=="Yes" or notes=="yes" or notes=="y":
+        MyNote=input("Enter your notes and then hit enter.")
+    else:
+        print("Skipping notes...") 
+
+    
 #If the user chose 3 cards / past preset future
+
 elif int(Reading)== 3:
     print("Here are your past, present, and future cards: ")
-    cards=0
-    MyCards=[]
-
-#while the number of cards is less than three, draw and print a new card
-    while cards < 3:
+    #while the number of cards is less than three, draw and print a new card
+    if cards < 3:
        card=random.choice(MajorArcana)       
        MyCards.append(card)
        print(card)
-       cards=cards+1
-          
+       cards=cards+1          
+    elif cards==3:
+        notes=input("Would you like to add a note? Y/N:")
+        if notes=="Y" or notes=="Yes" or notes=="yes" or notes=="y":
+            MyNote=input("Enter your notes and then hit enter.")
+        else:
+            print("Skipping notes...") 
 else:
-    print("Please enter 1 or 3.")        
+    print("No cards are pulled today.")     
+       
+TodaysReading={
+ #   "Date":ThisDate,
+    "MyCards":MyCards,
+    "Notes":MyNote
+}
+   
 
+w = csv.writer(open( "MyReading.csv", "w"))
+for key, val in TodaysReading.items():
+    w.writerow([val])
+print("Your reading was written to My readings.csv")
+    
